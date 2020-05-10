8478f08088b491c7e2fdf20eb44bcd63bbe97d1c
alfonzjanfrithz <alfonzjanfrithz@gmail.com>
2020-05-10 17:19:22 +0800

    Refactor the component name

--- src/main/java/com/alfonzjanfrithz/todoboard/todoboardapi/controller/ProjectController.java
+++ src/main/java/com/alfonzjanfrithz/todoboard/todoboardapi/controller/ProjectController.java
@@ -1,17 +1,18 @@
 package com.alfonzjanfrithz.todoboard.todoboardapi.controller;
 
 import com.alfonzjanfrithz.project.todoboard.api.ProjectsApi;
-import com.alfonzjanfrithz.project.todoboard.model.Project;
+import com.alfonzjanfrithz.project.todoboard.model.ProjectResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import javax.validation.Valid;
 import java.util.Collections;
 import java.util.List;
 
 @Controller
 public class ProjectController implements ProjectsApi {
     @Override
-    public ResponseEntity<List<Project>> searchProjects() {
+    public ResponseEntity<List<ProjectResponse>> searchProjects(@Valid String name) {
         return ResponseEntity.ok(Collections.emptyList());
     }
 }
